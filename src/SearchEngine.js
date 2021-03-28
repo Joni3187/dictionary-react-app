5b854e81ed3d85bebf0b5d61d58abfc4bffe7c88
RobertaRocks <74051883+RobertaRocks@users.noreply.github.com>
2021-03-28 19:48:11 +0200

    Added Results and Meaning component

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,18 +1,21 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import './SearchEngine.css';
 
 export default function SearchEngine () {
 let [keyword, setKeyword] = useState("");
+let [results, setResults] = useState(null);
 
 
 function handleResponse (response) {
-console.log(response.data[0]);
+setResults(response.data[0]);
+}
 
-};
-
-function search (event) {
+function search(event) {
 event.preventDefault();
+
+// documenttion: https://dictionaryapi.dev/ //
 
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
@@ -35,7 +38,7 @@ return (
       <input type="search" onChange={handlekeywordChange} className="form-control in-line mr-sm" placeholder="Enter a Word" aria-label="Recipient's username" aria-describedby="button-addon2" />
       <button type="submit" className="btn btn-outline-secondary my-sm-0 searchbtn"> Search</button>
     </form>
-
+<Results results={results}/>
 </div>);
 
 
