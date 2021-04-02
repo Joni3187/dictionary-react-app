1407371a90ec451ad5725039f97c995280896b84
RobertaRocks <74051883+RobertaRocks@users.noreply.github.com>
2021-04-02 19:49:06 +0200

    Added Photos component

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import './SearchEngine.css';
 
 export default function SearchEngine (props) {
 let [keyword, setKeyword] = useState(props.defaultKeyword);
 let [results, setResults] = useState(null);
 let [loaded, setLoaded] = useState(false);
+let [photos, setPhotos] = useState(null);
+
 
 function load () {
 setLoaded(true);
@@ -23,6 +26,7 @@ return (
       <button type="submit" className="btn btn-outline-secondary my-sm-0 searchbtn"> Search</button>
     </form>
 <Results results={results}/>
+<Photos photos={photos}/>
 </div>);
 } else {
 load();
@@ -35,12 +39,23 @@ search();
 
 }
 
+function handlePexelsResponse (response) {
+setPhotos(response.data.photos);
+
+}
+
+
 function search () {
 
 // documenttion: https://dictionaryapi.dev/ //
 
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
+
+let pexelsApiKey ="563492ad6f9170000100000184c2b5205a2c423b9ff6a7841e947aa0" ;
+let pexelsApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+let headers = {Authorization: `Bearer ${pexelsApiKey}`};
+axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse);
 
 }
 
